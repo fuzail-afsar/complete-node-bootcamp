a6cd0afe9ae6b68a41741a018db35867426545ae
fuzailahmed <muhammadfuzailahmed@folio3.com>
2026-03-01 02:55:29 +0500

    added /api/v1/tours/:id route

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,7 +9,7 @@ const tours = JSON.parse(
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'sucess',
+    status: 'success',
     data: {
       tours,
     },
@@ -17,7 +17,7 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  const newTourId = tours[tours.length - 1].id;
+  const newTourId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newTourId }, req.body);
   tours.push(newTour);
 
@@ -26,13 +26,32 @@ app.post('/api/v1/tours', (req, res) => {
     JSON.stringify(tours),
     () => {
       res.status(201).json({
-        status: 'sucess',
+        status: 'success',
         data: {
           tour: newTour,
         },
       });
     },
   );
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
 });
 
 const port = 3000;
